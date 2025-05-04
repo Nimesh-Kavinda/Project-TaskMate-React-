561fc41482c6640413a40d93e3b710d8c092b99e
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-04 23:52:43 +0530

    Make Header and AddTask Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
+import { ShowTask } from './components/ShowTask';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello TaskMate</h1>
+      <Header/>
+      <AddTask/>
+      <ShowTask/>
     </div>
   );
 }
